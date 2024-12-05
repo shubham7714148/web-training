24c47edb21d6bb8a28a4602554893717f606e747
shubham7714148 <shubham7714148@gmail.com>
2024-12-05 15:56:43 +0530

    Add files via upload

---
+++ day-9/js index.js
@@ -0,0 +1,25 @@
+console.log(b);...
+
+const data = [...[
+]
+];
+const name = data[0].name;
+console.log("first value", name1);
+constcategory = data[0].categoryconst price = [0].price
+console.log("category=", category);
+console.log("price",price);
+const hmtData = <h1>${name</h1>
+
+for (leti=0; i<datai < data.length; i++) {
+    cont name1 = data[1].name;
+
+    element.innerHML += htmlDataElement;
+    
+}
+
+const data = [
+
+] 
+
+
+// 
