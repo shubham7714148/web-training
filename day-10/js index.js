bea35505e3ec9afc957d985989edf56826a447e2
shubham7714148 <shubham7714148@gmail.com>
2024-12-09 15:54:31 +0530

    day-10

---
+++ day-10/js index.js
@@ -0,0 +1,17 @@
+function Sum(a,b) {
+    sum=a+b;
+    console.log(sum;)
+}
+sum(4,6);
+
+const counter= document.getElementById("counter");
+
+let count = 0;
+function incrementValue() {
+    count--;
+    Counter.innerText = count;
+}
+function decValue() {
+    count--;
+    Counter.innerText = count;
+}
